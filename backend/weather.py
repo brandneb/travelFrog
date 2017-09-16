956339cdfc4e9b5f776c82cbb2315ccb6696ff7e
Robert Brünig <robert.bruenig@gmail.com>
2017-09-16 16:01:14 +0200

    untested weather v2

--- backend/weather.py
+++ backend/weather.py
@@ -1,17 +1,18 @@
 import aiohttp
 import asyncio
+from itertools import islice
 
 from backend.settings import settings
 
 API_KEY = settings['bluemix_api_key']
 
-_weather = {}
+_weather_map = {}
 
 
-def _chunks(list, chunk_len):
-    """Yield successive chun_len-sized chunks from list."""
-    for i in range(0, len(list), chunk_len):
-        yield list[i:i + chunk_len]
+def _key_chunks(dict, chunk_size):
+    it = iter(dict)
+    for i in range(0, len(dict), chunk_size):
+        yield [k for k in islice(it, chunk_size)]
 
 
 def _build_request_url(lat, lon) -> str:
@@ -23,33 +24,29 @@ async def _get_weather_at(lat, lon, session):
         return await resp.json()
 
 
-async def _init_location(lat, lon):
-    global _weather
-    async with aiohttp.ClientSession() as session:
-        _weather.append(await _get_weather_at(lat, lon, session))
-
-
-async def _get_weather_all_locations(session):
-    global _weather
-    _weather = []
+async def _get_weather_map(session):
+    global _weather_map
     print("refreshing weather cache")
-    for chunk in _chunks(_locations, settings['weather_parallel_requests']):
-        requests = map(lambda loc: _get_weather_at(loc.lat, loc.lon, session), chunk)
-        _weather.extend(await asyncio.gather(*requests))
+    for chunk in _key_chunks(_weather_map, settings['weather_parallel_requests']):
+        requests = map(lambda coord: _get_weather_at(coord[0], coord[1], session), chunk)
+        weather_chunk = await asyncio.gather(*requests)
+        for i, coord in enumerate(chunk):
+            _weather_map[coord] = weather_chunk[i]
     print("done refreshing weather cache")
 
 
 async def run_weather_cache():
     async with aiohttp.ClientSession() as session:
         while asyncio.get_event_loop().is_running():
-            await _get_weather_all_locations(session)
+            await _get_weather_map(session)
             await asyncio.sleep(settings['weather_refresh_interval_s'])
 
 
-def get_weather(lat, lon):
-    if (lat, lon) in _weather:
-        return _weather[(lat, lon)]
+async def get_weather(lat, lon):
+    if (lat, lon) in _weather_map:
+        return _weather_map[(lat, lon)]
 
-    _locations[(lat, lon)] = name
-    asyncio.ensure_future(_init_location(lat, lon))
-
+    async with aiohttp.ClientSession() as session:
+        weather = _get_weather_at(lat, lon, session)
+        _weather_map[(lat, lon)] = weather
+        return weather
