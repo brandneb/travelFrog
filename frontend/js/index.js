7904ede222a7770fdc8aa7b3997bcd9629f5c39e
Liesa <liesa.weigert@tum.de>
2017-09-16 09:50:32 +0200

    Just added further elements to the index page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,3 +2,12 @@ $(document).ready(function() {
     console.log($('#nav').height(), window.innerHeight);
     $('.screensize').css({height: window.innerHeight - $('#nav').height()});
 });
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 2, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+});
