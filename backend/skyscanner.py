422f5c86b2188a7586ea05a207e9cc2b6cc05bdb
Felix Sonntag <felix.sonntag@outlook.com>
2017-09-16 20:19:06 +0200

    Changed booking URL generation

--- backend/skyscanner.py
+++ backend/skyscanner.py
@@ -13,6 +13,7 @@ API_KEY = settings['skyscanner_api_key']
 ROUTES_URL = "http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/GE/EUR/EN/{0}/{1}/anytime/anytime?apiKey={2}"
 SUGGEST_URL = "http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/GE/EUR/EN/?query={0}&apiKey={1}"
 REFERRAL_URL = "http://partners.api.skyscanner.net/apiservices/referral/v1.0/GE/EUR/EN/{0}/{1}/2017-09-22/2017-09-24?apiKey={2}"
+BOOKING_URL = "https://www.skyscanner.net/transport/flights/{0}/{1}/170922/170924"
 
 with open('data/continent_countries_mapping.json') as data_file:
     CONTINENTS_COUNTRIES_MAP = json.load(data_file)
@@ -23,7 +24,7 @@ async def get_url(url, session):
         return await resp.json()
 
 
-async def _get_cheapest_places_from_place_to_countries(from_city, to_countries, num_places):
+async def _get_cheapest_places_from_place_to_countries(from_city, to_countries, num_places, with_url):
     urls = map(lambda country: ROUTES_URL.format(from_city, country, API_KEY), to_countries)
 
     async with aiohttp.ClientSession() as session:
@@ -31,20 +32,20 @@ async def _get_cheapest_places_from_place_to_countries(from_city, to_countries, 
         for url in urls:
             responses.append(get_url(url, session))
         responses = await asyncio.gather(*responses)
-        cheapest_places_per_country = [_parse_routes_for_cheapest_destinations(response, num_places) for response in
-                                       responses]
+        cheapest_places_per_country = [await _parse_routes_for_cheapest_destinations(response, num_places, with_url)
+                                       for response in responses]
         cheapest_places = list(itertools.chain.from_iterable(cheapest_places_per_country))
         cheapest_places.sort(key=lambda x: x['price'])
         cheapest_places = cheapest_places[:num_places]
         return cheapest_places
 
 
-async def _get_cheapest_places_from_place_to_country(from_city, to_country, num_places):
+async def _get_cheapest_places_from_place_to_country(from_city, to_country, num_places, with_url):
     url = ROUTES_URL.format(from_city, to_country, API_KEY)
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             response_dict = await resp.json()
-            return _parse_routes_for_cheapest_destinations(response_dict, num_places)
+            return _parse_routes_for_cheapest_destinations(response_dict, num_places, with_url)
 
 
 async def _suggest_id(query):
@@ -55,7 +56,7 @@ async def _suggest_id(query):
             return response_dict['Places'][0]['PlaceId']
 
 
-def _parse_routes_for_cheapest_destinations(route_results, num_results):
+async def _parse_routes_for_cheapest_destinations(route_results, num_results, with_url):
     if 'ValidationErrors' in route_results:
         return []
     cheapest_places = []
@@ -66,7 +67,14 @@ def _parse_routes_for_cheapest_destinations(route_results, num_results):
 
     for route in all_routes:
         destination = _place_for_id(route['DestinationId'], all_places)
-        cheapest_places.append({'destination': destination, 'price': route['Price']})
+        if with_url:
+            origin = _place_for_id(route['OriginId'], all_places)
+            origin_iata = origin['IataCode']
+            destination_iata = destination['IataCode']
+            booking_url = BOOKING_URL.format(origin_iata, destination_iata)
+            cheapest_places.append({'destination': destination, 'price': route['Price'], 'booking_url': booking_url})
+        else:
+            cheapest_places.append({'destination': destination, 'price': route['Price']})
 
     return cheapest_places
 
@@ -84,15 +92,15 @@ async def _get_referral_url(from_id, to_id):
             return resp.headers['Location']
 
 
-async def get_destinations(from_city, to, num_destinations):
+async def get_destinations(from_city, to, num_destinations, with_url):
     from_id = await _suggest_id(from_city)
 
     if to.lower() in CONTINENTS_COUNTRIES_MAP:
         to_countries = CONTINENTS_COUNTRIES_MAP[to.lower()]
-        return await _get_cheapest_places_from_place_to_countries(from_id, to_countries, num_destinations)
+        return await _get_cheapest_places_from_place_to_countries(from_id, to_countries, num_destinations, with_url)
     else:
         to_id = await _suggest_id(to)
-        return await _get_cheapest_places_from_place_to_country(from_id, to_id, num_destinations)
+        return await _get_cheapest_places_from_place_to_country(from_id, to_id, num_destinations, with_url)
 
 
 async def get_booking_url(from_city, to_city):
